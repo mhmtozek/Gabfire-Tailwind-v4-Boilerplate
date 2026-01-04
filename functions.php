320c5196dada3acb3f79082554d73699ae19385e
mhmtozek <mhmtozek@gmail.com>
2026-01-04 03:22:38 +0200

    Boilerplate

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
 function gabfire_2026_assets() {
-wp_enqueue_style(
-'gabfire-style',
-get_template_directory_uri() . '/dist/css/style.css',
-[],
-filemtime(get_template_directory() . '/dist/css/style.css')
-);
+    $css_path = get_template_directory() . '/dist/css/style.css';
+    $css_uri  = get_template_directory_uri() . '/dist/css/style.css';
+
+    if (file_exists($css_path)) {
+        wp_enqueue_style(
+            'gabfire-2026-style',
+            $css_uri,
+            [],
+            filemtime($css_path)
+        );
+    }
 }
 add_action('wp_enqueue_scripts', 'gabfire_2026_assets');
